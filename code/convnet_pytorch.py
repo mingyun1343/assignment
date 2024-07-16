149c4856e4e146e968ed4235673a8c824e48dcc7
mingyun1343 <1343655109@qq.com>
2024-07-16 21:07:50 +0800

    updata

--- code/convnet_pytorch.py
+++ code/convnet_pytorch.py
@@ -6,15 +6,20 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import numpy as np
+import torch
+from torch import nn
+
+
 class ConvNet(nn.Module):
-  """
+    """
   This class implements a Convolutional Neural Network in PyTorch.
   It handles the different layers and parameters of the model.
   Once initialized an ConvNet object can perform forward.
   """
 
-  def __init__(self, n_channels, n_classes):
-    """
+    def __init__(self, n_channels, n_classes):
+        """
     Initializes ConvNet object. 
     
     Args:
@@ -26,16 +31,50 @@ class ConvNet(nn.Module):
     Implement initialization of the network.
     """
 
-    ########################
-    # PUT YOUR CODE HERE  #
-    #######################
-    raise NotImplementedError
-    ########################
-    # END OF YOUR CODE    #
-    #######################
+        ########################
+        # PUT YOUR CODE HERE  #
+        #######################
+        super(ConvNet, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(n_channels, 64, kernel_size=3, stride=1, padding=1),  # conv1
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),  # maxpool1
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),  # conv2
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),  # maxpool2
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),  # conv3_a
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),  # conv3_b
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),  # maxpool3
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),  # conv4_a
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),  # conv4_b
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),  # maxpool4
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),  # conv5_a
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),  # conv5_b
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),  # maxpool5
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(512,n_classes)
+        )
+        ########################
+        # END OF YOUR CODE    #
+        #######################
 
-  def forward(self, x):
-    """
+    def forward(self, x):
+        """
     Performs forward pass of the input. Here an input tensor x is transformed through 
     several layer transformations.
     
@@ -48,12 +87,26 @@ class ConvNet(nn.Module):
     Implement forward pass of the network.
     """
 
-    ########################
-    # PUT YOUR CODE HERE  #
-    #######################
-    raise NotImplementedError
-    ########################
-    # END OF YOUR CODE    #
-    #######################
+        ########################
+        # PUT YOUR CODE HERE  #
+        #######################
+        x = self.features(x)
+        x = torch.flatten(x, 1)
+        out = self.classifier(x)
+        ########################
+        # END OF YOUR CODE    #
+        #######################
 
-    return out
+        return out
+
+
+# if __name__ == '__main__':
+#   random_data = np.random.rand(2, 3, 32, 32)  # 调整数据形状为 (batch_size, channels, height, width)
+#   random_data_tensor = torch.from_numpy(random_data.astype(np.float32))  # 将NumPy数组转换为PyTorch的Tensor类型，并确保数据类型为float32
+#   print("输入数据的数据维度", random_data_tensor.size())  # 检查数据形状是否正确
+#
+#   # 创建VGG16网络实例
+#   model = ConvNet(3,10)
+#   output = model(random_data_tensor)
+#   print("输出数据维度", output.shape)
+#   print(output)
--- code/train_convnet_pytorch.py
+++ code/train_convnet_pytorch.py
@@ -9,6 +9,13 @@ from __future__ import print_function
 import argparse
 import numpy as np
 import os
+
+import torch
+import torchvision
+from matplotlib import pyplot as plt
+from matplotlib.pyplot import imshow
+from torchvision.transforms import transforms
+
 from convnet_pytorch import ConvNet
 import cifar10_utils
 
@@ -20,12 +27,13 @@ EVAL_FREQ_DEFAULT = 500
 OPTIMIZER_DEFAULT = 'ADAM'
 
 # Directory in which cifar data is saved
-DATA_DIR_DEFAULT = './cifar10/cifar-10-batches-py'
+DATA_DIR_DEFAULT = './cifar10'
 
 FLAGS = None
 
+
 def accuracy(predictions, targets):
-  """
+    """
   Computes the prediction accuracy, i.e. the average of correct predictions
   of the network.
   
@@ -42,69 +50,124 @@ def accuracy(predictions, targets):
   Implement accuracy computation.
   """
 
-  ########################
-  # PUT YOUR CODE HERE  #
-  #######################
-  raise NotImplementedError
-  ########################
-  # END OF YOUR CODE    #
-  #######################
+    ########################
+    # PUT YOUR CODE HERE  #
+    #######################
+    predicted_classes = torch.argmax(predictions, dim=1)
+    true_classes = torch.argmax(targets, dim=1)
+    correct_predictions = torch.sum(predicted_classes == true_classes)
+    accuracy = correct_predictions.float() / targets.size(0)
+    ########################
+    # END OF YOUR CODE    #
+    #######################
 
-  return accuracy
+    return accuracy
+
 
 def train():
-  """
+    """
   Performs training and evaluation of ConvNet model. 
 
   TODO:
   Implement training and evaluation of ConvNet model. Evaluate your model on the whole test set each eval_freq iterations.
   """
 
-  ### DO NOT CHANGE SEEDS!
-  # Set the random seeds for reproducibility
-  np.random.seed(42)
+    ### DO NOT CHANGE SEEDS!
+    # Set the random seeds for reproducibility
+    np.random.seed(42)
 
-  ########################
-  # PUT YOUR CODE HERE  #
-  #######################
-  raise NotImplementedError
-  ########################
-  # END OF YOUR CODE    #
-  #######################
+    ########################
+    # PUT YOUR CODE HERE  #
+    #######################
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    # 加载训练数据集
+    trainset = torchvision.datasets.CIFAR10(root=DATA_DIR_DEFAULT, train=True,
+                                            download=False, transform=transform)
+
+    # 创建训练数据加载器
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                              shuffle=True, num_workers=2)
+
+    # 加载测试数据集
+    testset = torchvision.datasets.CIFAR10(root=DATA_DIR_DEFAULT, train=False,
+                                           download=False, transform=transform)
+
+    # 创建测试数据加载器
+    testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                             shuffle=False, num_workers=2)
+    classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+    model = ConvNet(3,10)  # 替换为你的模型
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+    for epoch in range(2):  # 训练两个周期
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # 获取输入；数据是一个[inputs, labels]列表
+            inputs, labels = data
+
+            # 清零参数梯度
+            optimizer.zero_grad()
+            # print(inputs.shape)
+            # 正向传播，反向传播，优化
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # 打印统计信息
+            running_loss += loss.item()
+            if i % 2000 == 1999:  # 每2000个批次打印一次
+                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+                running_loss = 0.0
+    # digit = trainloader.dataset.data[0]
+    # plt.imshow(digit, cmap=plt.cm.binary)
+    # plt.show()
+    # print(classes[trainloader.dataset.targets[0]])
+    ########################
+    # END OF YOUR CODE    #
+    #######################
+
 
 def print_flags():
-  """
+    """
   Prints all entries in FLAGS variable.
   """
-  for key, value in vars(FLAGS).items():
-    print(key + ' : ' + str(value))
+    for key, value in vars(FLAGS).items():
+        print(key + ' : ' + str(value))
+
 
 def main():
-  """
+    """
   Main function
   """
-  # Print all Flags to confirm parameter settings
-  print_flags()
+    # Print all Flags to confirm parameter settings
+    print_flags()
 
-  if not os.path.exists(FLAGS.data_dir):
-    os.makedirs(FLAGS.data_dir)
+    if not os.path.exists(FLAGS.data_dir):
+        os.makedirs(FLAGS.data_dir)
 
-  # Run the training operation
-  train()
+    # Run the training operation
+    train()
+
 
 if __name__ == '__main__':
-  # Command line arguments
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--learning_rate', type = float, default = LEARNING_RATE_DEFAULT,
-                      help='Learning rate')
-  parser.add_argument('--max_steps', type = int, default = MAX_STEPS_DEFAULT,
-                      help='Number of steps to run trainer.')
-  parser.add_argument('--batch_size', type = int, default = BATCH_SIZE_DEFAULT,
-                      help='Batch size to run trainer.')
-  parser.add_argument('--eval_freq', type=int, default=EVAL_FREQ_DEFAULT,
+    # Command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--learning_rate', type=float, default=LEARNING_RATE_DEFAULT,
+                        help='Learning rate')
+    parser.add_argument('--max_steps', type=int, default=MAX_STEPS_DEFAULT,
+                        help='Number of steps to run trainer.')
+    parser.add_argument('--batch_size', type=int, default=BATCH_SIZE_DEFAULT,
+                        help='Batch size to run trainer.')
+    parser.add_argument('--eval_freq', type=int, default=EVAL_FREQ_DEFAULT,
                         help='Frequency of evaluation on the test set')
-  parser.add_argument('--data_dir', type = str, default = DATA_DIR_DEFAULT,
-                      help='Directory for storing input data')
-  FLAGS, unparsed = parser.parse_known_args()
+    parser.add_argument('--data_dir', type=str, default=DATA_DIR_DEFAULT,
+                        help='Directory for storing input data')
+    FLAGS, unparsed = parser.parse_known_args()
 
-  main()
+    main()
